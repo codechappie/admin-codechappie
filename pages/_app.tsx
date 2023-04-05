4aec7c0563c81825762ed0f1905ead76f614c5bd
CodeChappie <acanalesa14@gmail.com>
2023-04-05 00:57:53 -0500

    upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import { Router } from "next/dist/client/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+NProgress.configure({ showSpinner: false });
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+export default MyApp;
