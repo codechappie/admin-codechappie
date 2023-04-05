5d11b70eccc798b6be79bc4f500ec0b9adc714c0
CodeChappie <acanalesa14@gmail.com>
2023-04-05 00:20:12 -0500

    upload v6

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const removeImports = require("next-remove-imports")({});
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+}
 
-module.exports = removeImports({
-  reactStrictMode: true,
-  images: {
-    domains: ["icons8.com", "localhost", "res.cloudinary.com"],
-  },
-});
+module.exports = nextConfig
