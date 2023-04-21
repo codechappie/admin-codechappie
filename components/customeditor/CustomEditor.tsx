f0c87b66092ede072cbb8f975781e19e99a69f1d
CodeChappie <acanalesa14@gmail.com>
2023-04-21 15:16:30 -0500

    auth added

--- components/customeditor/CustomEditor.tsx
+++ components/customeditor/CustomEditor.tsx
@@ -21,7 +21,7 @@ const CustomEditor = ({ html, setHtml, leftlabel }: Props) => {
       <label htmlFor="">{leftlabel}</label>
       <Editor
         id="Editor"
-        apiKey="4i60cn1d7yypl7aysrm5drvcc46jiainltvvdijgx5hgymra"
+        apiKey={process.env.TINY_KEY}
         value={contenido}
         init={initFullProps}
         scriptLoading={{ delay: 0 }}
